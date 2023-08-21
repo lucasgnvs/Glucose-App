c1a2636a709b85a80cd86c046d27a7b0a04be837
Egluco <egluco@MacBook-Air-de-Egluco.local>
2023-08-21 14:19:46 -0300

    updated flutter

--- gluco/ios/Flutter/flutter_export_environment.sh
+++ gluco/ios/Flutter/flutter_export_environment.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\Users\User\AppData\Local\FlutterSDK\flutter"
-export "FLUTTER_APPLICATION_PATH=D:\Lucas\UDESC\Quinta Fase 2022-1\Bolsa Bertemes\App EGlico\Gluco\Glucose-App\gluco"
+export "FLUTTER_ROOT=/Users/egluco/development/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/egluco/Documents/egluco/e-Gluco-App/gluco"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
-export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
-export "FLUTTER_BUILD_NAME=0.6.6"
-export "FLUTTER_BUILD_NUMBER=16"
+export "FLUTTER_BUILD_NAME=0.7.0"
+export "FLUTTER_BUILD_NUMBER=1"
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=true"
 export "TREE_SHAKE_ICONS=false"
